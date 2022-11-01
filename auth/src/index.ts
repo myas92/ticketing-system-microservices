a56d9512d7f8b933b45e83767852acf909182cc7
yaser ahmadi <m.y.ahmadi22@gmail.com>
2022-11-01 14:45:19 +0330

    [improvement]: added app for start writing tests

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,36 +1,6 @@
-import { config } from 'dotenv';
-import express from "express";
-import 'express-async-errors'
-import { json } from "body-parser";
+
 import mongoose from "mongoose";
-import cookieSession from 'cookie-session'
-
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
-config()
-const app = express();
-// بصورت پیش فرض وقتی درخواست از با اچ تی تی پی اس بیاد اکسپرس ردش میکنه
-app.set('trust proxy', true); // ترافیک از طریق پروکسی اینگرس میاد
-app.use(json());
-app.use(cookieSession({
-  signed: false, // Because JWT is signed with SHA-256
-  secure: process.env.HTTPS == "true" ? true : false // visited user the app HTTPS connection
-}))
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all('/*', async () => {
-  throw new NotFoundError()
-})
-
-app.use(errorHandler);
+import { app } from './app'
 
 
 const start = async () => {
