959d65a707c33d9abf5ff5a45e5769fec3f0d749
yaser ahmadi <m.y.ahmadi22@gmail.com>
2022-11-02 12:45:18 +0330

    [feature]: added signu

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,25 +1,26 @@
-// inclued this path to the jest config in package.json
-
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
-
 import { app } from '../app';
 
 let mongo: any;
 beforeAll(async () => {
-    mongo = new MongoMemoryServer();
-    const mongoUri = await mongo.getUri();
-    await mongoose.connect(mongoUri);
+    mongo = await MongoMemoryServer.create();
+    const mongoUri = mongo.getUri();
+console.log(mongoUri)
+    await mongoose.connect(mongoUri, {
+
+    });
 });
 
 beforeEach(async () => {
     const collections = await mongoose.connection.db.collections();
+
     for (let collection of collections) {
-        await collection.deleteMany({})
+        await collection.deleteMany({});
     }
-})
+});
 
 afterAll(async () => {
     await mongo.stop();
     await mongoose.connection.close();
-})
+});
