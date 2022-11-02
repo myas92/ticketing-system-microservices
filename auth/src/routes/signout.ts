84b5d84c986e479ec8ea2e23a8686a56d52a1b96
yaser ahmadi <m.y.ahmadi22@gmail.com>
2022-11-02 14:41:35 +0330

    [improvement]: added signout test

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -4,7 +4,8 @@ const router = express.Router();
 
 router.post('/api/users/signout', (req, res) => {
   req.session = null;
-  res.send({})
+
+  res.send({});
 });
 
 export { router as signoutRouter };
