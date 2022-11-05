29c873024edf4c122d21744209cc3aaa787f4202
yaser ahmadi <m.y.ahmadi22@gmail.com>
2022-11-05 10:13:55 +0330

    [feature]: test for not authenticated

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest';
 import { app } from '../../app';
 
 it('responds with details about the current user', async () => {
-  
+
   const cookie = await global.signin()
   const response = await request(app)
     .get('/api/users/currentuser')
@@ -12,3 +12,13 @@ it('responds with details about the current user', async () => {
 
   expect(response.body.currentUser.email).toEqual('test@test.com');
 });
+
+it('responds with null if not authenticated', async () => {
+
+  const response = await request(app)
+  .get('/api/users/currentuser')
+  .send()
+  .expect(200)
+
+  expect(response.body.currentUser).toEqual(null) 
+})
