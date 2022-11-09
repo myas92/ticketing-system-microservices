d832d98af46d407e0cc4557c2b7224b38d2bdb01
yaser ahmadi <m.y.ahmadi22@gmail.com>
2022-11-09 12:10:12 +0330

    [improvement]: added hooks to signup form

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,15 +1,40 @@
+import { useState } from "react";
 export default () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    return (<form>
-        <h1>Sign Up</h1>
-        <div className="mb-3">
-            <label for="email" className="form-label">Email Address</label>
-            <input className="form-control" id="email" />
-        </div>
-        <div className="mb-3">
-            <label for="password" className="form-label">Password</label>
-            <input type="password" className="form-control" id="password"/>
-        </div>
-        <button className="btn btn-primary">Sing Up</button>
-    </form>);
+  const onSubmitHandler = (event) => {
+    event.preventDefault(0);
+    console.log(email, password);
+  };
+
+  return (
+    <form onSubmit={onSubmitHandler}>
+      <h1>Sign Up</h1>
+      <div className="mb-3">
+        <label className="form-label">
+          Email Address
+        </label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="form-control"
+          id="email"
+        />
+      </div>
+      <div className="mb-3">
+        <label className="form-label">
+          Password
+        </label>
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          className="form-control"
+          id="password"
+        />
+      </div>
+      <button className="btn btn-primary">Sing Up</button>
+    </form>
+  );
 };
