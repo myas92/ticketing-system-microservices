075f8a6acdf802472974f3c1da76e2d9cf1c449f
yaser ahmadi <m.y.ahmadi22@gmail.com>
2022-11-12 17:05:04 +0330

    [feature]: added sign in page

---
+++ client/pages/auth/signin.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import useRequest from "../../hooks/use-request";
+import Router from "next/router";
+export default () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { doRequest, errors } = useRequest({
+    url: "/api/users/signin",
+    method: "post",
+    body: {
+      email,
+      password,
+    },
+    onSuccess:()=>Router.push("/")
+  });
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+    await doRequest();
+  };
+
+  return (
+    <form onSubmit={onSubmitHandler}>
+      <h1>Sign In</h1>
+      <div className="mb-3">
+        <label className="form-label">Email Address</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="form-control"
+          id="email"
+        />
+      </div>
+      <div className="mb-3">
+        <label className="form-label">Password</label>
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          className="form-control"
+          id="password"
+        />
+      </div>
+      {errors}
+      <button className="btn btn-primary">Sing In</button>
+    </form>
+  );
+};
--- client/pages/banana.js
+++
@@ -1,5 +0,0 @@
-const Banana = () => {
-    return <h1>Bananaaassss</h1>
-}
-
-export default Banana;
