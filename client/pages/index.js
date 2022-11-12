e29407ae8126ce83b2d0147fc67f4fb093971281
yaser ahmadi <m.y.ahmadi22@gmail.com>
2022-11-12 16:43:09 +0330

    [feature]: added build client for getInitialProps

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,17 @@
-const Loading = () => {
-    return <h1>Loadingggg</h1>
-}
+import axios from "axios";
+import buildClient from "../api/build-client";
 
+const LandingPage = ({ currentUser }) => {
+  // console.log(currentUser);
+  // axios.get('/api/users/currentuser');
+  console.log(currentUser);
 
-export default Loading;
+  return <h1>Landing Page</h1>;
+};
+
+LandingPage.getInitialProps = async (context) => {
+  const { data } = await buildClient(context).get("/api/users/currentuser");
+  return data;
+};
+
+export default LandingPage;
