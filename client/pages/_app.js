d16bd6da12be1f82d9b8278d56dbd4e61d0e5fb6
yaser ahmadi <m.y.ahmadi22@gmail.com>
2022-11-13 09:34:09 +0330

    [improvement]:added calling getInitialProps in appComponent

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,23 @@
-import 'bootstrap/dist/css/bootstrap.css'
+import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
 
-export default ({ Component, pageProps }) => {
-    return <Component {...pageProps} />
-}
+const AppComponent = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+AppComponent.getInitialProps = async (appContext) => {
+    console.log("APP Componentttttttttt")
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get("/api/users/currentuser");
+
+  let pageProps = {};
+  // برای اینکه تابع راه اندازی در لندیگ اجرا شود باید بصورت دستی کانفیگ کنیم
+  if(appContext.Component.getInitialProps)
+  pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+
+  console.log(pageProps);
+
+  return data;
+};
+
+export default AppComponent;
--- client/pages/index.js
+++ client/pages/index.js
@@ -2,7 +2,11 @@ import axios from "axios";
 import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
-  return currentUser ? <h1>You are signed in</h1> : <h1>You are NOT signed in</h1>
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
+  );
 };
 
 LandingPage.getInitialProps = async (context) => {
