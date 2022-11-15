a856fb7ab257b6a238317b3940280d31aefa1281
yaser ahmadi <m.y.ahmadi22@gmail.com>
2022-11-15 13:36:06 +0330

    Update

--- common/src/idnex.ts
+++ common/src/idnex.ts
@@ -6,8 +6,6 @@ export * from './errors/not-found-error';
 export * from './errors/request-validation-error';
 
 
-
-
 export * from './middlewares/current-user';
 export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
