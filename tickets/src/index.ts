9bc0163bedcb7b9515ce37ebbb0001e0db7be7a5
yaser ahmadi <m.y.ahmadi22@gmail.com>
2022-11-19 13:00:38 +0330

    [fix] init tickets service & init k8s tickets config

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -8,6 +8,9 @@ const start = async () => {
     if (!process.env.JWT_KEY) {
       throw new Error('JWT_KEY must be defined')
     }
+    if (!process.env.MONGO_URI) {
+      throw new Error('MONGO URI must be defined')
+    }
 
     console.log("----------------------")
     console.log(process.env.NODE_ENV)
@@ -18,7 +21,8 @@ const start = async () => {
 
     }
     else{
-      await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+      console.log(process.env.MONGO_URI)
+      await mongoose.connect(process.env.MONGO_URI);
 
     }
     console.log('Connected to MongoDb')
