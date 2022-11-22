b44a0de157de82cff18c946099ee4cde8f37e0a7
yaser ahmadi <m.y.ahmadi22@gmail.com>
2022-11-22 13:57:42 +0330

    [improvement]: writting test - validation tickets input

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -11,15 +11,28 @@ it('Can only be accessed if the user is signed in', async () => {
     await request(app).post('/api/tickets').send({}).expect(401)
 
 })
-it('Returns a status code other than 401 if the use signed in ', async () => {
+it('Returns a status code other than 401 if the user signed in ', async () => {
     const response = await request(app).post('/api/tickets').set('Cookie', global.signin()).send({});
     expect(response.statusCode).not.toEqual(401)
 })
 it('Returns an error if an invalid title is provided', async () => {
 
+    await request(app).post('/api/tickets').set('Cookie', global.signin()).send({
+        title: "",
+        price: 100
+    }).expect(400)
+    await request(app).post('/api/tickets').set('Cookie', global.signin()).send({
+        title: "",
+    }).expect(400)
 })
 it('Returns an error if an invelid price is provided', async () => {
-
+    await request(app).post('/api/tickets').set('Cookie', global.signin()).send({
+        title: "my title",
+        price: -100
+    }).expect(400)
+    await request(app).post('/api/tickets').set('Cookie', global.signin()).send({
+        title: "my title",
+    }).expect(400)
 })
 it('Create a tickets with valid inputs', async () => {
 
