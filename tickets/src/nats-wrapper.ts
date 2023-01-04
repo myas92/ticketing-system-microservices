9d995064a17e69f58e5d65b2a9d85407ce5ec88e
yaser ahmadi <m.y.ahmadi22@gmail.com>
2023-01-04 14:57:51 +0330

    [fix]: cleaned code

--- tickets/src/nats-wrapper.ts
+++ tickets/src/nats-wrapper.ts
@@ -8,6 +8,7 @@ class NatsWrapper {
         }
         return this._client;
     }
+    
     connect(clusterId: string, clientId: string, url: string) {
         this._client = nats.connect(clusterId, clientId, { url });
         return new Promise((resolve, reject) => {
