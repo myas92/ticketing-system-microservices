3c66561acaac678dd93ccbbf48b2b8825795ee4f
yaser ahmadi <m.y.ahmadi22@gmail.com>
2023-01-07 16:41:57 +0330

    [improvement]: init mock into new test

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import { app } from '../../app';
 import { Ticket } from '../../models/tickets';
 
+jest.mock('../../nats-wrapper')
 
 it('Has a route handler listening to api/tickets for post requests', async () => {
 
