c362253757b9e5d9d47e908325476eae929d2416
yaser ahmadi <m.y.ahmadi22@gmail.com>
2023-01-21 09:49:17 +0330

    [improvement]: added mock for nats-wrapper

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -3,11 +3,11 @@ import mongoose from 'mongoose';
 import jwt from 'jsonwebtoken'
 import { app } from '../app';
 import request from 'supertest';
-
 declare global {
     function signin(): string[];
 }
 
+jest.mock('../nats-wrapper')
 
 let mongo: any;
 beforeAll(async () => {
