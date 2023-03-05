41e9cdf7d7b2e9ee284a6e078489fbc7b7c60638
yaser ahmadi <m.y.ahmadi22@gmail.com>
2023-03-05 11:15:24 +0330

    [fix]: fixed routes in app file

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -5,10 +5,10 @@ import { json } from "body-parser";
 import cookieSession from 'cookie-session'
 import { errorHandler, NotFoundError, currentUser } from "@myasticketing/common";
 
-import { createTicketRouter } from "./routes/new";
-import { showTicketRouter } from './routes/show';
-import { indexTicketRouter } from './routes';
-import { updateTicketRouter } from './routes/update';
+import { newOrderRouter } from "./routes/new";
+import { showOrderRouter } from './routes/show';
+import { indexOrderRouter } from './routes';
+import { deleteOrderRouter } from './routes/delete';
 
 config()
 const app = express();
@@ -22,10 +22,10 @@ app.use(cookieSession({
 
 app.use(currentUser);
 
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+app.use(indexOrderRouter);
+app.use(deleteOrderRouter);
 
 
 app.all('*', async () => {
