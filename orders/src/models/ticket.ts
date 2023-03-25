d67af9f230c3445e1a682811632e4c38a8d51748
yaser ahmadi <m.y.ahmadi22@gmail.com>
2023-03-25 10:00:45 +0330

    [fix]: fixed id adjustment for ticket listener

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import { Order, OrderStatus } from './order';
 interface TicketAttrs {
+    id: string,
     title: string,
     price: Number
 }
@@ -35,25 +36,29 @@ const ticketSchema = new mongoose.Schema({
 })
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
-    return new Ticket(attrs)
+    return new Ticket({
+        _id: attrs.id,
+        title: attrs.title,
+        price: attrs.price,
+    })
 }
 
 ticketSchema.methods.isReserved = async function () {
     // this === the ticket document that we just called 'isReserved' on
     const existingOrder = await Order.findOne({
-      ticket: this as any,
-      status: {
-        $in: [
-          OrderStatus.Created,
-          OrderStatus.AwaitingPayment,
-          OrderStatus.Complete,
-        ],
-      },
+        ticket: this as any,
+        status: {
+            $in: [
+                OrderStatus.Created,
+                OrderStatus.AwaitingPayment,
+                OrderStatus.Complete,
+            ],
+        },
     });
-  
+
     return !!existingOrder;
-  };
-  
+};
+
 
 const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema)
 
